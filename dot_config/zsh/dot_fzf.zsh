1f3dbe4aa111cc785d165f4960bed4e096e3400f
Jose Luis Cambero <jo@m1>
2023-02-27 14:17:47 +0100

    Fix paths

--- dot_config/zsh/dot_fzf.zsh
+++ dot_config/zsh/dot_fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/jlcambero/.config/zsh/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/jlcambero/.config/zsh/fzf/bin"
+if [[ ! "$PATH" == *$ZDOTDIR/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$ZDOTDIR/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/jlcambero/.config/zsh/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$ZDOTDIR/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/jlcambero/.config/zsh/fzf/shell/key-bindings.zsh"
+source "$ZDOTDIR/fzf/shell/key-bindings.zsh"
